d4c1d08b41c491ae0d5f49ca1642b50b1977a532
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-06-09 21:32:16 +0300

    Enhances category page with hover details and styling updates
    Adds hover functionality to display category details on mouseover,
    including title, image, and separator. Adjusts layout and styling
    for improved responsiveness and visual consistency. Updates
    category card interactions with hover effects and transitions.

--- src/pages/Category/CategoryPage.jsx
+++ src/pages/Category/CategoryPage.jsx
@@ -1,18 +1,33 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import Css from "./CategoryPage.module.css";
-
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { categorySelector } from "../../redux/category";
 
 const CategoryPage = () => {
   const selectCategories = useSelector(categorySelector.selectCategories);
+  const [selectCategory, setSelectCategory] = useState(null);
+
+  const hoverCard = (category) => setSelectCategory(category);
+
   return (
     <div className={Css.Page}>
       <Navbar />
+      {selectCategory && (
+        <div className={Css.HoverCategory}>
+          <h1 className={Css.HoverTitle}>{selectCategory.name}</h1>
+          <hr className={Css.HoverHr} />
+          <img
+            src={selectCategory.icon}
+            alt="Hover Category"
+            className={Css.HoverImage}
+          />
+        </div>
+      )}
       <div className={Css.Content}>
         <div className={Css.Header}>
           <p className={Css.Description}>Please select one of the categories</p>
-          <h1 className={Css.Title}>Category</h1>
+          <h2 className={Css.Title}>Category</h2>
         </div>
         <ul
           className={Css.CategoryList}
@@ -23,7 +38,13 @@ const CategoryPage = () => {
         >
           {selectCategories &&
             selectCategories.map(({ id, name, icon }, index) => (
-              <CategoryCard key={index} id={id} name={name} icon={icon} />
+              <CategoryCard
+                key={index}
+                id={id}
+                name={name}
+                icon={icon}
+                onHover={hoverCard}
+              />
             ))}
         </ul>
       </div>
@@ -31,9 +52,12 @@ const CategoryPage = () => {
   );
 };
 
-const CategoryCard = ({ id, name, icon }) => {
+const CategoryCard = ({ id, name, icon, onHover }) => {
   return (
-    <li className={Css.CategoryCard}>
+    <li
+      className={Css.CategoryCard}
+      onMouseOverCapture={() => onHover({ name, icon })}
+    >
       <div className={Css.CardDetail}>
         <p className={Css.CardDescription}>#{id}</p>
         <h2 className={Css.CardTitle}>{name}</h2>
